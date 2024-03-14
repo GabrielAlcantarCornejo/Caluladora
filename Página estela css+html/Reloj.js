fb0adec4f541c1e1bdc50e0732a6b129d92a283a
GabrielAlcantarCornejo <137913881+GabrielAlcantarCornejo@users.noreply.github.com>
2024-03-13 21:55:54 -0600

    Add files via upload

---
+++ Página estela css+html/Reloj.js
@@ -0,0 +1,24 @@
+function currentTime(){
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    hh=(hh<10) ? "0" + hh : hh;
+    mm=(mm<10) ? "0" + mm : mm;
+    ss=(ss<10) ? "0" + ss : ss;
+
+    let time = hh + ":" + mm + ":" + ss;
+    let watch = document.querySelector('#Digital')
+    watch.innerHTML = time;
+
+    let hhRotation = ((hh & 12)+180)/6;
+    let mmRotation = (mm + 180)/30;
+    let ssRotation = (ss + 180)/30;
+
+    document.querySelector('Hours').computedStyleMap.transform = "rotate("+hhRotation+"deg)";
+    document.querySelector('Minutes').computedStyleMap.transform = "rotate("+mmRotation+"deg)";
+    document.querySelector('Seconds').computedStyleMap.transform = "rotate("+ssRotation+"deg)";
+}
+
+setInterval(currentTime, 1000);
